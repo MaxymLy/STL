7b4cdc1484e2a685b2f384f1f39afb11de9a6662
MaxymLy <jakbyjabuvkonem@gmail.com>
2022-04-30 13:24:38 +0300

    revert

--- coini_reports.py
+++ coini_reports.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 import openpyxl
 
+
 def coini():
     st.write('Everything')
     uploaded_file = st.file_uploader("Upload CSV", type=".xlsx")
 
     if uploaded_file:
-        df = pd.read_excel(uploaded_file, index_col=2)
+        df = pd.read_excel(uploaded_file)
         st.dataframe(df)
-
