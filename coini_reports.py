2a18f08ae991ecfa3a9573c382160b016f0a7564
MaxymLy <jakbyjabuvkonem@gmail.com>
2022-04-30 13:06:22 +0300

    567567

--- coini_reports.py
+++ coini_reports.py
@@ -5,9 +5,7 @@ def coini():
     st.write('Everything')
     uploaded_file = st.file_uploader("Upload")
 
-    filename = 'dogs.pickle'
-    outfile = open(filename, 'wb')
+    df = pd.read_excel(uploaded_file, index_col=3)
 
-    pickle.dump(uploaded_file, outfile)
-    outfile.close()
+    st.dataframe(df)
 
