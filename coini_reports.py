3950165ccdb338f37125db34cc64c947f8556376
MaxymLy <jakbyjabuvkonem@gmail.com>
2022-04-30 13:07:25 +0300

    567567

--- coini_reports.py
+++ coini_reports.py
@@ -5,7 +5,8 @@ def coini():
     st.write('Everything')
     uploaded_file = st.file_uploader("Upload")
 
-    df = pd.read_excel(uploaded_file, index_col=3)
+    if uploaded_file:
+        df = pd.read_excel(uploaded_file, index_col=3)
 
     st.dataframe(df)
 
